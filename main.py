2c8a134615adde803ee5ab215fa4c6742e241b0e
Sanket Zanwar <shankii@Sankets-MacBook-Air.local>
2025-02-17 23:38:03 -0500

    Added Data Validation step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.MLOPS_Project import logger
 from src.MLOPS_Project.pipeline.DataIngestion import DataIngestionTrainingPipeline
+from src.MLOPS_Project.pipeline.DataValidation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -8,6 +9,19 @@ if __name__ == "__main__":
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
         obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.initiate_data_validation()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
--- src/MLOPS_Project/config/config.py
+++ src/MLOPS_Project/config/config.py
@@ -1,5 +1,8 @@
 from src.MLOPS_Project.constants import *
-from src.MLOPS_Project.entity.config_entity import DataIngestionConfig
+from src.MLOPS_Project.entity.config_entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+)
 from src.MLOPS_Project.utils.common import create_directories, read_yaml
 
 
@@ -28,3 +31,18 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir,
         )
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
