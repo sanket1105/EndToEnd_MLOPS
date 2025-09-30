c15dc50a22558703775e8d494f422556dacda24a
Sanket Zanwar <shankii@Sankets-MacBook-Air.local>
2025-09-30 10:48:25 -0500

    script.py added

--- config/dummy.py
+++ config/dummy.py
@@ -1,89 +1,87 @@
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
-from sklearn.metrics import (
-    accuracy_score,
-    f1_score,
-    log_loss,
-    matthews_corrcoef,
-    precision_score,
-    recall_score,
-    roc_auc_score,
-)
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from tqdm import tqdm
+
+np.seterr(divide="ignore", invalid="ignore")
 
 
-class TransformationEvaluator:
-    def __init__(self, X: pd.DataFrame, y: pd.Series, scoring_method: str) -> float:
-        self.X = X.copy(deep=True)
-        self.y = y.copy(deep=True)
-        self.scoring_method = scoring_method
+class ReduceVIF:
+    def __init__(self, data: pd.DataFrame, threshold: int = 10, verbose: bool = False):
+        self.data = data
+        self.threshold = threshold
+        self.verbose = verbose
 
-        assert scoring_method in (
-            "accuracy",
-            "precision",
-            "recall",
-            "auc",
-            "f1",
-            "log_loss",
-            "r2",
-            "matthews",
-        ), "scoring method must be one of accuracy, precision, recall, auc, f1, log loss, r2, matthews"
+    def calculate_vif_chunks(self, X: pd.DataFrame):
+        vif = pd.DataFrame()
+        vif["Features"] = X.columns
+        for i, col in enumerate(X.columns):
+            if X[col].nunique() == 1:
+                vif.at[i, "VIF"] = -1
+            else:
+                vif_value = variance_inflation_factor(X.values, i)
+                vif.at[i, "VIF"] = vif_value
 
-    def train(self):
-        self.X = sm.add_constant(self.X)
-        model = sm.Logit(self.y, self.X).fit()
-        self.r2 = model.prsquared
+        return vif
 
-        self.model = model
+    def calculate_vif(self, X: pd.DataFrame):
+        vif = pd.DataFrame()
+        vif["Features"] = X.columns
+        pbar = tqdm(
+            total=len(X.columns), desc="Calculating VIF", position=0, leave=True
+        )
+        for i, col in enumerate(X.columns):
+            if X[col].nunique() == 1:
+                vif.at[i, "VIF"] = -1
+            else:
+                vif_value = variance_inflation_factor(X.values, i)
+                vif.at[i, "VIF"] = vif_value
 
-    def predict(self):
-        pos_prob = self.model.predict(self.X)
-        neg_prob = 1 - pos_prob
-        preds = round(pos_prob)
-        # probs = np.column_stack((neg_prob, pos_prob))
-        probs = pos_prob
-        self.preds = preds
-        self.probs = probs
+            pbar.update(1)
+        pbar.close()
 
-    def score(self):
-        y_true = self.y
-        y_pred = self.preds
-        y_prob = self.probs
-        if self.scoring_method == "accuracy":
-            return accuracy_score(y_true, y_pred)
-        elif self.scoring_method == "precision":
-            return precision_score(y_true, y_pred)
-        elif self.scoring_method == "recall":
-            return recall_score(y_true, y_pred)
-        elif self.scoring_method == "f1":
-            return f1_score(y_true, y_pred)
-        elif self.scoring_method == "auc":
-            return roc_auc_score(y_true, y_prob)
-        elif self.scoring_method == "log_loss":
-            return log_loss(y_true, y_prob)
-        elif self.scoring_method == "r2":
-            return self.r2
-        elif self.scoring_method == "matthews":
-            return matthews_corrcoef(y_true, y_pred)
+        vif = vif.sort_values(by="VIF", ascending=False)
+        return vif
+
+    def _vif_chunks(self, threshold: int):
+        width = self.data.shape[1]
+        cols_to_keep = []
+        for i in tqdm(
+            range(self.data.shape[1] // 50 + 1),
+            desc="Processing Chunks",
+            position=0,
+            leave=True,
+            dynamic_ncols=True,
+        ):
+            start_idx = i * 50
+            end_idx = (i + 1) * 50
+            df = self.data.iloc[:, start_idx:end_idx]
+            df_vif = self.calculate_vif_chunks(df)
+            df_vif = df_vif[(df_vif.VIF <= threshold) & (df_vif.VIF > -1)]
+            feats_to_keep = list(df_vif.Features)
+            cols_to_keep += feats_to_keep
+
+        self.data = self.data.loc[:, cols_to_keep]
+
+        if self.verbose:
+            print(f"removed {width - self.data.shape[1]} features")
 
     def run(self):
-        self.train()
-        self.predict()
-        model_score = self.score()
+        self._vif_chunks(100)
+        self._vif_chunks(50)
+        self._vif_chunks(25)
 
-        return model_score
+        max_vif = 100
+        while max_vif > self.threshold:
+            vif = self.calculate_vif(self.data)
+            # remove infinity ones
+            vif = vif[vif.VIF != np.inf]
+            vif = vif.reset_index(drop=True)
+            # keep all features below threshold or with index of 10+, ensures to aren't removing too many at once
+            vif = vif[
+                ((vif.VIF <= self.threshold) | (vif.index >= 25)) & (vif.VIF > -1)
+            ]
+            max_vif = vif.VIF.max()
+            self.data = self.data.loc[:, vif.Features]
 
-
-if __name__ == "__main__":
-    data = pd.DataFrame(
-        {"grade_nominal": [75, 90, 80, 83, 100], "passing": [0, 1, 1, 0, 1]}
-    )
-    X = data.iloc[:, :1]
-    y = data.iloc[:, -1]
-    TransformationEvaluator(X, y, "accuracy").run()
-    TransformationEvaluator(X, y, "precision").run()
-    TransformationEvaluator(X, y, "recall").run()
-    TransformationEvaluator(X, y, "f1").run()
-    TransformationEvaluator(X, y, "log_loss").run()
-    TransformationEvaluator(X, y, "r2").run()
-    TransformationEvaluator(X, y, "matthews").run()
+        return self.data, vif
