493fc1ab53b680576af09fc13d0c2062b234ceba
Sanket Zanwar <shankii@Sankets-MacBook-Air.local>
2025-09-30 11:23:35 -0500

    script.py added

--- config/dummy.py
+++ config/dummy.py
@@ -1,87 +1,73 @@
-import numpy as np
 import pandas as pd
-from statsmodels.stats.outliers_influence import variance_inflation_factor
-from tqdm import tqdm
-
-np.seterr(divide="ignore", invalid="ignore")
+from sklearn.preprocessing import KBinsDiscretizer
 
 
-class ReduceVIF:
-    def __init__(self, data: pd.DataFrame, threshold: int = 10, verbose: bool = False):
-        self.data = data
-        self.threshold = threshold
-        self.verbose = verbose
+class BinData:
+    def __init__(
+        self,
+        X_train,
+        X_valid,
+        binning_cols,
+        non_binning_cols,
+        n_bins,
+        bin_encoding,
+        bin_strategy,
+        output_path,
+    ):
+        self.X_train = X_train
+        self.X_valid = X_valid
+        self.binning_cols = binning_cols
+        self.non_binning_cols = non_binning_cols
+        self.n_bins = n_bins
+        self.bin_encoding = bin_encoding
+        self.bin_strategy = bin_strategy
+        self.output_path = output_path
 
-    def calculate_vif_chunks(self, X: pd.DataFrame):
-        vif = pd.DataFrame()
-        vif["Features"] = X.columns
-        for i, col in enumerate(X.columns):
-            if X[col].nunique() == 1:
-                vif.at[i, "VIF"] = -1
-            else:
-                vif_value = variance_inflation_factor(X.values, i)
-                vif.at[i, "VIF"] = vif_value
+    def split_data(self):
+        self.X_train_bin = self.X_train.loc[:, self.binning_cols]
+        self.X_train_non_bin = self.X_train.loc[:, self.non_binning_cols]
+        self.X_valid_bin = self.X_valid.loc[:, self.binning_cols]
+        self.X_valid_non_bin = self.X_valid.loc[:, self.non_binning_cols]
 
-        return vif
-
-    def calculate_vif(self, X: pd.DataFrame):
-        vif = pd.DataFrame()
-        vif["Features"] = X.columns
-        pbar = tqdm(
-            total=len(X.columns), desc="Calculating VIF", position=0, leave=True
+    def bin_data(self):
+        est = KBinsDiscretizer(
+            n_bins=self.n_bins, encode=self.bin_encoding, strategy=self.bin_strategy
         )
-        for i, col in enumerate(X.columns):
-            if X[col].nunique() == 1:
-                vif.at[i, "VIF"] = -1
-            else:
-                vif_value = variance_inflation_factor(X.values, i)
-                vif.at[i, "VIF"] = vif_value
+        X_train_kbins = pd.DataFrame.sparse.from_spmatrix(
+            est.fit_transform(self.X_train_bin)
+        )
+        X_valid_kbins = pd.DataFrame.sparse.from_spmatrix(
+            est.transform(self.X_valid_bin)
+        )
 
-            pbar.update(1)
-        pbar.close()
+        self.est = est
+        self.X_train_kbins = X_train_kbins
+        self.X_valid_kbins = X_valid_kbins
 
-        vif = vif.sort_values(by="VIF", ascending=False)
-        return vif
+    def update_columns(self):
+        new_cols = []
+        for j in range(len(self.binning_cols)):
+            for k in range(len(self.est.bin_edges_[j]) - 1):
+                base = self.binning_cols[j]
+                lower_val = round(self.est.bin_edges_[j][k], 4)
+                upper_val = round(self.est.bin_edges_[j][k + 1], 4)
+                col_name = f"{base}_{lower_val}_{upper_val}"
+                new_cols.append(col_name)
+        self.X_train_kbins.columns = new_cols
+        self.X_valid_kbins.columns = new_cols
 
-    def _vif_chunks(self, threshold: int):
-        width = self.data.shape[1]
-        cols_to_keep = []
-        for i in tqdm(
-            range(self.data.shape[1] // 50 + 1),
-            desc="Processing Chunks",
-            position=0,
-            leave=True,
-            dynamic_ncols=True,
-        ):
-            start_idx = i * 50
-            end_idx = (i + 1) * 50
-            df = self.data.iloc[:, start_idx:end_idx]
-            df_vif = self.calculate_vif_chunks(df)
-            df_vif = df_vif[(df_vif.VIF <= threshold) & (df_vif.VIF > -1)]
-            feats_to_keep = list(df_vif.Features)
-            cols_to_keep += feats_to_keep
-
-        self.data = self.data.loc[:, cols_to_keep]
-
-        if self.verbose:
-            print(f"removed {width - self.data.shape[1]} features")
+    def add_non_transformed_data(self):
+        self.X_train_kbins = pd.concat(
+            [self.X_train_non_bin, self.X_train_kbins], axis=1
+        )
+        self.X_valid_kbins = pd.concat(
+            [self.X_valid_non_bin, self.X_valid_kbins], axis=1
+        )
 
     def run(self):
-        self._vif_chunks(100)
-        self._vif_chunks(50)
-        self._vif_chunks(25)
+        self.split_data()
+        self.bin_data()
+        self.update_columns()
+        self.add_non_transformed_data()
 
-        max_vif = 100
-        while max_vif > self.threshold:
-            vif = self.calculate_vif(self.data)
-            # remove infinity ones
-            vif = vif[vif.VIF != np.inf]
-            vif = vif.reset_index(drop=True)
-            # keep all features below threshold or with index of 10+, ensures to aren't removing too many at once
-            vif = vif[
-                ((vif.VIF <= self.threshold) | (vif.index >= 25)) & (vif.VIF > -1)
-            ]
-            max_vif = vif.VIF.max()
-            self.data = self.data.loc[:, vif.Features]
-
-        return self.data, vif
+        return self.X_train_kbins, self.X_valid_kbins, self.est
