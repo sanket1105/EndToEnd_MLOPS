72ec4b069a3efb13f310e5fd207d71cb5cb5500b
Sanket Zanwar <shankii@Sankets-MacBook-Air.local>
2025-09-30 10:21:48 -0500

    script.py added

--- config/dummy.py
+++ config/dummy.py
@@ -1,170 +1,89 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import (
-    FunctionTransformer,
-    MinMaxScaler,
-    PowerTransformer,
-    RobustScaler,
-    StandardScaler,
+import statsmodels.api as sm
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    log_loss,
+    matthews_corrcoef,
+    precision_score,
+    recall_score,
+    roc_auc_score,
 )
 
 
-class DataTransformer:
-    def __init__(
-        self,
-        train: pd.DataFrame,
-        valid: pd.DataFrame,
-        cols: list,
-        transformations: list,
-    ):
-        """
-        Initialize the DataTransformer.
+class TransformationEvaluator:
+    def __init__(self, X: pd.DataFrame, y: pd.Series, scoring_method: str) -> float:
+        self.X = X.copy(deep=True)
+        self.y = y.copy(deep=True)
+        self.scoring_method = scoring_method
 
-        :param train: Training DataFrame.
-        :param valid: Validation DataFrame.
-        :param cols: List of columns to transform.
-        :param transformations: List of transformation names.
-        """
-        self.train = train
-        self.valid = valid
-        self.cols = cols
-        self.transformations = transformations
-        self.transformations_dict = {
-            "log": FunctionTransformer(np.log, validate=True),
-            "sqrt": FunctionTransformer(np.sqrt, validate=True),
-            "reciprocal": FunctionTransformer(np.reciprocal, validate=True),
-            "exponential": FunctionTransformer(lambda x: x**2, validate=True),
-            "yeo_johnson": PowerTransformer(
-                method="yeo-johnson", standardize=False, copy=False
-            ),
-        }
+        assert scoring_method in (
+            "accuracy",
+            "precision",
+            "recall",
+            "auc",
+            "f1",
+            "log_loss",
+            "r2",
+            "matthews",
+        ), "scoring method must be one of accuracy, precision, recall, auc, f1, log loss, r2, matthews"
 
-        self.transformer_objects = {}
-        self.transformer_details = {}
-        self.small_constant = 1e-8  # Defined constant for log and reciprocal
+    def train(self):
+        self.X = sm.add_constant(self.X)
+        model = sm.Logit(self.y, self.X).fit()
+        self.r2 = model.prsquared
 
-    def fill_nans_with_mean(self, df):
-        """Fill NaNs with column means."""
-        return df.fillna(df.mean())
+        self.model = model
 
-    def apply_transformation(self, transformer):
-        func = self.transformations_dict[transformer]
+    def predict(self):
+        pos_prob = self.model.predict(self.X)
+        neg_prob = 1 - pos_prob
+        preds = round(pos_prob)
+        # probs = np.column_stack((neg_prob, pos_prob))
+        probs = pos_prob
+        self.preds = preds
+        self.probs = probs
 
-        train_filled = self.fill_nans_with_mean(self.train[self.cols].copy(deep=True))
-        valid_filled = self.fill_nans_with_mean(self.valid[self.cols].copy(deep=True))
-
-        if transformer in ["log", "reciprocal"]:
-            train_filled += self.small_constant
-            valid_filled += self.small_constant
-
-        # Fit the PowerTransformer for Yeo-Johnson
-        if transformer == "yeo_johnson":
-            func.fit(train_filled)
-            self.transformer_objects[transformer] = func
-            self.transformer_details[transformer] = dict(zip(self.cols, func.lambdas_))
-
-        df_train = pd.DataFrame(func.transform(train_filled), columns=self.cols)
-        df_valid = pd.DataFrame(func.transform(valid_filled), columns=self.cols)
-
-        # Replace NaNs that came from log and sqrt transformations
-        if transformer in ["log", "sqrt"]:
-            df_train.fillna(0, inplace=True)
-            df_valid.fillna(0, inplace=True)
-
-        self.train = pd.concat(
-            [self.train, df_train.add_suffix(f"_{transformer}")], axis=1
-        )
-        self.valid = pd.concat(
-            [self.valid, df_valid.add_suffix(f"_{transformer}")], axis=1
-        )
-
-    def apply_scaler(self, transformer):
-        scaler = {
-            "min_max": MinMaxScaler(),
-            "standard": StandardScaler(),
-            "robust": RobustScaler(),
-        }[transformer.replace("_scaler", "")]
-
-        df_train = pd.DataFrame(
-            scaler.fit_transform(self.train[self.cols]), columns=self.cols
-        ).add_suffix(f"_{transformer}")
-        df_valid = pd.DataFrame(
-            scaler.transform(self.valid[self.cols]), columns=self.cols
-        ).add_suffix(f"_{transformer}")
-
-        self.train = pd.concat([self.train, df_train], axis=1)
-        self.valid = pd.concat([self.valid, df_valid], axis=1)
-
-        self.transformer_details[transformer] = {
-            col_name: (
-                {"min": scaler.data_min_, "max": scaler.data_max_}
-                if transformer == "min_max_scaler"
-                else (
-                    {"mu": scaler.mean_, "std": scaler.scale_}
-                    if transformer == "standard_scaler"
-                    else {"median": scaler.center_, "iqr": scaler.scale_}
-                )
-            )
-            for col_name in self.cols
-        }
-        self.transformer_objects[transformer] = scaler
+    def score(self):
+        y_true = self.y
+        y_pred = self.preds
+        y_prob = self.probs
+        if self.scoring_method == "accuracy":
+            return accuracy_score(y_true, y_pred)
+        elif self.scoring_method == "precision":
+            return precision_score(y_true, y_pred)
+        elif self.scoring_method == "recall":
+            return recall_score(y_true, y_pred)
+        elif self.scoring_method == "f1":
+            return f1_score(y_true, y_pred)
+        elif self.scoring_method == "auc":
+            return roc_auc_score(y_true, y_prob)
+        elif self.scoring_method == "log_loss":
+            return log_loss(y_true, y_prob)
+        elif self.scoring_method == "r2":
+            return self.r2
+        elif self.scoring_method == "matthews":
+            return matthews_corrcoef(y_true, y_pred)
 
     def run(self):
-        for t in self.transformations:
-            if "_scaler" not in t:
-                self.apply_transformation(t)
-            else:
-                self.apply_scaler(t)
+        self.train()
+        self.predict()
+        model_score = self.score()
 
-        nominal_cols = [c + "_nominal" for c in self.cols]
-        self.train.rename(columns=dict(zip(self.cols, nominal_cols)), inplace=True)
-        self.valid.rename(columns=dict(zip(self.cols, nominal_cols)), inplace=True)
-
-        return (
-            self.train,
-            self.valid,
-            self.transformer_objects,
-            self.transformer_details,
-        )
+        return model_score
 
 
 if __name__ == "__main__":
-    train = pd.DataFrame(
-        {
-            "name": ["Alice", "Bob", "Carol"],
-            "age": [25, 30, 35],
-            "grade": [75, 90, 80],
-            "is_senior": [1, 0, 1],
-        }
+    data = pd.DataFrame(
+        {"grade_nominal": [75, 90, 80, 83, 100], "passing": [0, 1, 1, 0, 1]}
     )
-    valid = pd.DataFrame(
-        {
-            "name": ["Ian", "Mari"],
-            "age": [35, 0],
-            "grade": [np.nan, 95],
-            "is_senior": [1, 0],
-        }
-    )
-    transformation_cols = ["age", "grade"]
-    transformations = [
-        "log",
-        "sqrt",
-        "exponential",
-        "reciprocal",
-        "yeo_johnson",
-        "min_max_scaler",
-        "standard_scaler",
-        "robust_scaler",
-    ]
-
-    train_scaled, valid_scaled, trans_objects, trans_details = DataTransformer(
-        train=train,
-        valid=valid,
-        cols=transformation_cols,
-        transformations=transformations,
-    ).run()
-
-    print(train_scaled)
-    print(valid_scaled)
-    print(trans_objects)
-    print(trans_details)
+    X = data.iloc[:, :1]
+    y = data.iloc[:, -1]
+    TransformationEvaluator(X, y, "accuracy").run()
+    TransformationEvaluator(X, y, "precision").run()
+    TransformationEvaluator(X, y, "recall").run()
+    TransformationEvaluator(X, y, "f1").run()
+    TransformationEvaluator(X, y, "log_loss").run()
+    TransformationEvaluator(X, y, "r2").run()
+    TransformationEvaluator(X, y, "matthews").run()
